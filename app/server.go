3968b0e8027286a49da5adcdc1ca0ba6c61a8665
Raiven Kao <omegaatt36@gmail.com>
2024-02-16 15:13:29 +0800

    feat: response pong.

--- app/server.go
+++ app/server.go
@@ -2,22 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		log.Println("New connection from: ", conn.RemoteAddr().String())
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		log.Println("Error writing to connection: ", err.Error())
 	}
 }
